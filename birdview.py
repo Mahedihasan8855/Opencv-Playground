319df54f3a9b0777b65ec81e4581cdc273eaeabe
Mahedi Hassan <mahedihassan8855@gmail.com>
2021-04-10 16:08:45 +0600

    wrap Prespective

--- birdview.py
+++ birdview.py
@@ -2,14 +2,16 @@ import cv2 as cv
 import numpy as np
 path="image/king.jpg"
 king=cv.imread(path,1)
-king=cv.resize(king,(600,400),None,1,1)
+
+
+pts1=np.float32([[178,52],[324,49],[233,204],[408,192]]) #image pixels of 4 point
+pts2=np.float32([[0,0],[300,0],[0,400],[300,400]])# setting the image
+matrix=cv.getPerspectiveTransform(pts1,pts2)
+image_output=cv.warpPerspective(king,matrix,(300,400))
 
 
 
 
-
-
-
-cv.imshow("original",king)
+cv.imshow("warp_Prespective",image_output)
 cv.waitKey(0)
 print(king.shape)
